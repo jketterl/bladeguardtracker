751d9c6a5f820419e5eb01d1bb16e415940f779d
Jakob Ketterl <jketterl@chipxonio.de>
2011-07-04 16:17:05 +0200

    add toggle button & enable location listener only when toggled

--- src/net/djmacgyver/bgl/BladeGuardLocatorActivity.java
+++ src/net/djmacgyver/bgl/BladeGuardLocatorActivity.java
@@ -6,7 +6,10 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class BladeGuardLocatorActivity extends Activity {
     /** Called when the activity is first created. */
@@ -15,8 +18,9 @@ public class BladeGuardLocatorActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        LocationManager m = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        m.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+        final LocationManager m = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        final LocationListener l = new LocationListener() {
 			
 			@Override
 			public void onStatusChanged(String provider, int status, Bundle extras) {
@@ -40,6 +44,19 @@ public class BladeGuardLocatorActivity extends Activity {
 			public void onLocationChanged(Location location) {
 				Toast.makeText(getApplicationContext(), "Location changed: " + location.getLatitude() + " " + location.getLongitude(), Toast.LENGTH_SHORT).show();
 			}
+		};
+        
+        ToggleButton b = (ToggleButton) findViewById(R.id.toggleButton1);
+        b.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (((ToggleButton) v).isChecked()) {
+					m.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, l);
+				} else {
+					m.removeUpdates(l);
+				}
+			}
 		});
+        
     }
 }
