7d620661388b5c31296717632d83c96e7e76af58
Jakob Ketterl <jketterl@chipxonio.de>
2011-07-19 15:39:42 +0200

    simpler

--- src/net/djmacgyver/bgt/Map.java
+++ src/net/djmacgyver/bgt/Map.java
@@ -1,16 +1,12 @@
 package net.djmacgyver.bgt;
 
-import java.util.List;
-
-import com.google.android.maps.MapActivity;
-import com.google.android.maps.MapView;
-import com.google.android.maps.Overlay;
-
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
 public class Map extends MapActivity {
-	private List<Overlay> mapOverlays;
 	private UserOverlay users;
 	private Drawable drawable;
 	
@@ -21,14 +17,13 @@ public class Map extends MapActivity {
     	MapView v = (MapView) findViewById(R.id.mapview);
     	v.setBuiltInZoomControls(true);
     	
-    	mapOverlays = v.getOverlays();
     	drawable = this.getResources().getDrawable(R.drawable.icon);
     	users = new UserOverlay(drawable);
     	
     	MapClient c = new MapClient(users, getApplicationContext());
     	c.start();
     	
-    	mapOverlays.add(users);
+    	v.getOverlays().add(users);
     }
 
 	@Override
