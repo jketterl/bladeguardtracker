e0e5f52eeef6989f0dc144188705cf79e688d161
Jakob Ketterl <jketterl@chipxonio.de>
2011-07-19 23:34:15 +0200

    use a special ssl socket factory, that accepts self-signed certificates

--- src/net/djmacgyver/bgt/http/HttpClient.java
+++ src/net/djmacgyver/bgt/http/HttpClient.java
@@ -1,5 +1,20 @@
 package net.djmacgyver.bgt.http;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
 import net.djmacgyver.bgt.R;
 
 import org.apache.http.conn.ClientConnectionManager;
@@ -17,6 +32,38 @@ import android.content.Context;
 import android.content.pm.PackageManager.NameNotFoundException;
 
 public class HttpClient extends DefaultHttpClient {
+	private class EasySSLSocketFactory extends SSLSocketFactory {
+	    SSLContext sslContext = SSLContext.getInstance("TLS");
+
+	    public EasySSLSocketFactory(KeyStore truststore) throws NoSuchAlgorithmException, KeyManagementException, KeyStoreException, UnrecoverableKeyException {
+	        super(truststore);
+
+	        TrustManager tm = new X509TrustManager() {
+	            public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+	            }
+
+	            public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+	            }
+
+	            public X509Certificate[] getAcceptedIssuers() {
+	                return null;
+	            }
+	        };
+
+	        sslContext.init(null, new TrustManager[] { tm }, null);
+	    }
+
+	    @Override
+	    public Socket createSocket(Socket socket, String host, int port, boolean autoClose) throws IOException, UnknownHostException {
+	        return sslContext.getSocketFactory().createSocket(socket, host, port, autoClose);
+	    }
+
+	    @Override
+	    public Socket createSocket() throws IOException {
+	        return sslContext.getSocketFactory().createSocket();
+	    }
+	}
+
 	private Context context;
 	
 	public HttpClient(Context context)	{
@@ -29,9 +76,34 @@ public class HttpClient extends DefaultHttpClient {
 		SchemeRegistry schreg = new SchemeRegistry();
 		schreg.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
 		
-		final SSLSocketFactory sslSocketFactory = SSLSocketFactory.getSocketFactory();
-        sslSocketFactory.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
-        schreg.register(new Scheme("https", sslSocketFactory, 443));
+ 		try {
+			KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+			trustStore.load(null, null);
+
+	        final SSLSocketFactory sslSocketFactory = new EasySSLSocketFactory(trustStore);
+
+	        //final SSLSocketFactory sslSocketFactory = SSLSocketFactory.getSocketFactory();
+	        sslSocketFactory.setHostnameVerifier(SSLSocketFactory.BROWSER_COMPATIBLE_HOSTNAME_VERIFIER);
+	        schreg.register(new Scheme("https", sslSocketFactory, 443));
+		} catch (KeyStoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (CertificateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (KeyManagementException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnrecoverableKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         
         return new ThreadSafeClientConnManager(this.createHttpParams(), schreg);
 	}
