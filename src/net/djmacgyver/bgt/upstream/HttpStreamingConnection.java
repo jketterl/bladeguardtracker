d3442cba53bee0b89065c10fca93b8ee35ada8ad
Jakob Ketterl <jketterl@chipxonio.de>
2011-07-24 01:07:58 +0200

    limiting update frequency (1/10s)

--- src/net/djmacgyver/bgt/upstream/HttpStreamingConnection.java
+++ src/net/djmacgyver/bgt/upstream/HttpStreamingConnection.java
@@ -1,10 +1,16 @@
 package net.djmacgyver.bgt.upstream;
 
+import net.djmacgyver.bgt.keepalive.KeepAliveTarget;
+import net.djmacgyver.bgt.keepalive.KeepAliveThread;
 import android.content.Context;
 import android.location.Location;
 
-public class HttpStreamingConnection implements Connection {
+public class HttpStreamingConnection implements Connection, KeepAliveTarget {
 	private HttpStreamingThread thread;
+	private KeepAliveThread gpsReminder;
+	private boolean updateBlocked = false;
+	private Location queuedLocation;
+	private Location lastLocation;
 	
 	private HttpStreamingThread getThread() {
 		if (thread == null) {
@@ -16,18 +22,30 @@ public class HttpStreamingConnection implements Connection {
 
 	@Override
 	public void connect() {
+		getGpsReminder().start();
 	}
 
 	@Override
 	public void disconnect() {
+		getGpsReminder().terminate();
 		getThread().terminate();
 	}
 
 	@Override
 	public void sendLocation(Location location) {
+		if (location.equals(lastLocation)) return;
+		if (updateBlocked) {
+			queuedLocation = location;
+			return;
+		}
+
 		String data = "lat=" + location.getLatitude() + "&lon=" + location.getLongitude();
 		if (location.hasSpeed()) data += "&speed=" + location.getSpeed();
 		getThread().sendData(data);
+		
+		getGpsReminder().interrupt();
+		lastLocation = location;
+		updateBlocked = true;
 	}
 
 	@Override
@@ -38,5 +56,27 @@ public class HttpStreamingConnection implements Connection {
 	@Override
 	public void sendQuit() {
 		getThread().sendData("quit");
+	}
+
+	@Override
+	public void keepAlive(KeepAliveThread source) {
+		if (source == getGpsReminder()) {
+			updateBlocked = false;
+			sendLocation();
+		}
+	}
+
+	private void sendLocation() {
+		if (queuedLocation == null) return;
+		Location l = queuedLocation;
+		queuedLocation = null;
+		sendLocation(l);
+	}
+
+	private KeepAliveThread getGpsReminder() {
+		if (gpsReminder == null) {
+			gpsReminder = new KeepAliveThread(this, 10);
+		}
+		return gpsReminder;
 	}
 }
