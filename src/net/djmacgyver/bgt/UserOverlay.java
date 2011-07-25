c8a4f0682935c525173391742df4fb30a897ef43
Jakob Ketterl <jketterl@chipxonio.de>
2011-07-25 14:13:23 +0200

    synchronizing in order to get rid of the annoying arrayindexoutofboundsexception

--- src/net/djmacgyver/bgt/UserOverlay.java
+++ src/net/djmacgyver/bgt/UserOverlay.java
@@ -2,10 +2,12 @@ package net.djmacgyver.bgt;
 
 import java.util.HashMap;
 
+import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.MapView;
 import com.google.android.maps.OverlayItem;
 
 public class UserOverlay extends ItemizedOverlay<OverlayItem> {
@@ -17,21 +19,24 @@ public class UserOverlay extends ItemizedOverlay<OverlayItem> {
 		populate();
 	}
 	
-	public void updateUser(int userId, GeoPoint point) {
+	public synchronized void updateUser(int userId, GeoPoint point) {
 		OverlayItem i = mOverlays.get(userId);
 		if (i != null) mOverlays.remove(i);
 		mOverlays.put(userId, new OverlayItem(point, "", ""));
+		setLastFocusedIndex(-1);
 		populate();
 	}
 	
-	public void removeUser(int userId) {
+	public synchronized void removeUser(int userId) {
 		mOverlays.remove(userId);
+		setLastFocusedIndex(-1);
 		populate();
 	}
 	
-	public void reset()
+	public synchronized void reset()
 	{
 		mOverlays.clear();
+		setLastFocusedIndex(-1);
 		populate();
 	}
 
@@ -43,5 +48,11 @@ public class UserOverlay extends ItemizedOverlay<OverlayItem> {
 	@Override
 	public int size() {
 		return mOverlays.size();
+	}
+
+	@Override
+	public synchronized void draw(Canvas canvas, MapView mapView, boolean shadow) {
+		// TODO Auto-generated method stub
+		super.draw(canvas, mapView, shadow);
 	}
 }
