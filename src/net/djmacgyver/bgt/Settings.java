a64a22a48b2658d27699251bfe0d0fb56c077121
Jakob Ketterl <jketterl@chipxonio.de>
2011-07-26 22:58:12 +0200

    preparing signup activity

--- src/net/djmacgyver/bgt/Settings.java
+++ src/net/djmacgyver/bgt/Settings.java
@@ -1,6 +1,8 @@
 package net.djmacgyver.bgt;
 
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
 import android.view.Window;
 import android.widget.TextView;
@@ -15,5 +17,14 @@ public class Settings extends PreferenceActivity {
         TextView t = (TextView) findViewById(R.id.title);
         t.setText(R.string.settings);
         addPreferencesFromResource(R.xml.settings);
+        
+        Preference signup = findPreference("signup");
+        signup.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+			@Override
+			public boolean onPreferenceClick(Preference preference) {
+				System.out.println("signup clicked");
+				return true;
+			}
+		});
 	}
 }
