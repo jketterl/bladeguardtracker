2f21792bca83b268900276ea821011a68592b90a
Jakob Ketterl <jketterl@chipxonio.de>
2011-07-26 16:09:44 +0200

    add first setting

--- src/net/djmacgyver/bgt/Settings.java
+++ src/net/djmacgyver/bgt/Settings.java
@@ -1,20 +1,20 @@
 package net.djmacgyver.bgt;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 import android.view.Window;
 import android.widget.TextView;
 
-public class Settings extends Activity {
+public class Settings extends PreferenceActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
-        setContentView(R.layout.settings);
+        super.onCreate(savedInstanceState);
+        
         getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.header);
         TextView t = (TextView) findViewById(R.id.title);
         t.setText(R.string.settings);
+        addPreferencesFromResource(R.xml.settings);
 	}
-	
 }
