f1bc10f0de2ee55e0d0c5ed876b70a681f668893
Jakob Ketterl <jketterl@chipxonio.de>
2011-07-27 11:28:36 +0200

    dialogs for the user signup process

--- src/net/djmacgyver/bgt/Signup.java
+++ src/net/djmacgyver/bgt/Signup.java
@@ -10,6 +10,10 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
@@ -20,6 +24,10 @@ import android.view.Window;
 import android.widget.TextView;
 
 public class Signup extends PreferenceActivity {
+	static final int DIALOG_SIGNUP_RUNNING = 0;
+	static final int DIALOG_SIGNUP_SUCCESSFUL = 1;
+	static final int DIALOG_SIGNUP_FAILED = 2;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
         requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
@@ -40,22 +48,80 @@ public class Signup extends PreferenceActivity {
 				if (p.getString("username", "").equals("")) return false;
 				if (p.getString("password", "").equals("")) return false;
 				if (!p.getString("password", "a").equals(p.getString("password_confirm", "b"))) return false;
+				
+				showDialog(DIALOG_SIGNUP_RUNNING);
+				
 				HttpClient c = new HttpClient(getApplicationContext());
 				HttpPost req = new HttpPost(Config.baseUrl + "signup");
 				try {
 					req.setEntity(new StringEntity("user=" + p.getString("username", null) + "&pass=" + p.getString("password", "")));
 					HttpResponse res = c.execute(req);
+					dismissDialog(DIALOG_SIGNUP_RUNNING);
 					if (res.getStatusLine().getStatusCode() != 200) {
 						HttpEntity e = res.getEntity();
 						BufferedReader in = new BufferedReader(new InputStreamReader(e.getContent()));
 						String line = null;
-						while ((line = in.readLine()) != null) System.out.println(line);
+						String message = "";
+						while ((line = in.readLine()) != null) message = message.concat(line);
+						Bundle b = new Bundle();
+						b.putString("message", message);
+						showDialog(DIALOG_SIGNUP_FAILED, b);
+					} else {
+						showDialog(DIALOG_SIGNUP_SUCCESSFUL);
 					}
 				} catch (Exception e) {
+					dismissDialog(DIALOG_SIGNUP_RUNNING);
 					e.printStackTrace();
 				}
+				
 				return true;
 			}
 		});
+	}
+
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		Dialog d;
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		switch (id) {
+			case DIALOG_SIGNUP_RUNNING:
+				d = new ProgressDialog(this);
+				d.setTitle(R.string.signup_progress);
+				break;
+			case DIALOG_SIGNUP_SUCCESSFUL:
+				builder.setMessage(R.string.signup_successful)
+					   .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialog, int which) {
+								dialog.dismiss();
+							}
+					   });
+				d = builder.create();
+				break;
+			case DIALOG_SIGNUP_FAILED:
+				builder.setMessage(R.string.signup_failed)
+					   .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialog, int which) {
+								dialog.dismiss();
+							}
+					   });
+				d = builder.create();
+				break;
+			default:
+				d = super.onCreateDialog(id);
+		}
+		return d;
+	}
+
+	@Override
+	protected void onPrepareDialog(int id, Dialog dialog, Bundle args) {
+		switch (id) {
+			case DIALOG_SIGNUP_FAILED:
+				AlertDialog d = (AlertDialog) dialog;
+				String message = getResources().getString(R.string.signup_failed);
+				d.setMessage(message.concat("\n\n" + args.getString("message")));
+		}
+		super.onPrepareDialog(id, dialog, args);
 	}
 }
