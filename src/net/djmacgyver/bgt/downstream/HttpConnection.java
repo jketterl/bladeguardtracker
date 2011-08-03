c470b28bb27161346f5f6e803bc1a5ebb2d2c63e
Jakob Ketterl <jketterl@chipxonio.de>
2011-08-03 11:07:33 +0200

    refactor into methods

--- src/net/djmacgyver/bgt/downstream/HttpConnection.java
+++ src/net/djmacgyver/bgt/downstream/HttpConnection.java
@@ -44,12 +44,31 @@ public class HttpConnection extends Thread {
 	private Context context;
 	private boolean terminate = false;
 	private Handler length;
+	private XPath xPath;
+	private XPathExpression pointExpression;
+	private XPathExpression userExpression;
+	private XPathExpression locationExpression;
+	private XPathExpression quitExpression;
+	private XPathExpression mapExpression;
+	private XPathExpression statsExpression;
 	
 	public HttpConnection(UserOverlay users, RouteOverlay route, Context context, Handler h) {
 		this.users = users;
 		this.context = context;
 		this.route = route;
 		this.length = h;
+		
+		try {
+			userExpression = getXPath().compile("/updates/movements/user");
+			locationExpression = getXPath().compile("location[1]");
+			quitExpression = getXPath().compile("/updates/quit/user");
+			mapExpression = getXPath().compile("/updates/map[1]");
+			pointExpression = getXPath().compile("gpx:gpx/gpx:rte/gpx:rtept");
+			statsExpression = getXPath().compile("/updates/stats[1]");
+		} catch (XPathExpressionException e) {
+			e.printStackTrace();
+			terminate();
+		}
 	}
 	
 	private HttpClient getClient()
@@ -66,42 +85,6 @@ public class HttpConnection extends Thread {
 		dbf.setNamespaceAware(true);
 		DocumentBuilder builder;
 		InputSource in = new InputSource();
-		XPathFactory xfactory = XPathFactory.newInstance();
-		XPath x = xfactory.newXPath();
-		x.setNamespaceContext(new NamespaceContext() {
-			@Override
-			public Iterator<String> getPrefixes(String namespaceURI) {
-				return null;
-			}
-			
-			@Override
-			public String getPrefix(String namespaceURI) {
-				return null;
-			}
-			
-			@Override
-			public String getNamespaceURI(String prefix) {
-				if (prefix.equals("gpx")) return "http://www.topografix.com/GPX/1/1";
-				return null;
-			}
-		});
-		XPathExpression pointExpr;
-		XPathExpression userExpr;
-		XPathExpression locationExpr;
-		XPathExpression quitExpr;
-		XPathExpression mapExpr;
-		XPathExpression statsExpr;
-		try {
-			userExpr = x.compile("/updates/movements/user");
-			locationExpr = x.compile("location[1]");
-			quitExpr = x.compile("/updates/quit/user");
-			mapExpr = x.compile("/updates/map[1]");
-			pointExpr = x.compile("gpx:gpx/gpx:rte/gpx:rtept");
-			statsExpr = x.compile("/updates/stats[1]");
-		} catch (XPathExpressionException e2) {
-			e2.printStackTrace();
-			return;
-		}
 		try {
 			builder = dbf.newDocumentBuilder();
 		} catch (ParserConfigurationException e1) {
@@ -129,65 +112,7 @@ public class HttpConnection extends Thread {
 				in.setCharacterStream(new StringReader(xml));
 				Document dom = builder.parse(in);
 				
-				// get location updates
-				NodeList users = (NodeList) userExpr.evaluate(dom, XPathConstants.NODESET);
-				for (int i = 0; i < users.getLength(); i++) {
-					Node location = (Node) locationExpr.evaluate(users.item(i), XPathConstants.NODE);
-					int lat = 0, lon = 0;
-					for (int k = 0; k < location.getChildNodes().getLength(); k++) {
-						Node coord = location.getChildNodes().item(k);
-						int value = (int) (Float.parseFloat(coord.getTextContent()) * 1E6);
-						if (coord.getNodeName().equals("lat")) lat = value;
-						if (coord.getNodeName().equals("lon")) lon = value;
-					}
-					GeoPoint point = new GeoPoint(lat, lon);
-					Node user = users.item(i);
-					int userId = Integer.parseInt(user.getAttributes().getNamedItem("id").getNodeValue());
-					UserOverlayItem o = this.users.getUser(userId);
-					if (o != null) {
-						o.setPoint(point);
-					} else {
-						String userName = user.getAttributes().getNamedItem("name").getNodeValue();
-						String team = user.getAttributes().getNamedItem("team").getNodeValue();
-						this.users.addUser(new UserOverlayItem(point, userId, userName, team));
-					}
-				}
-				
-				// get removals
-				users = (NodeList) quitExpr.evaluate(dom, XPathConstants.NODESET);
-				for (int i = 0; i < users.getLength(); i++) {
-					int userId = Integer.parseInt(users.item(i).getAttributes().getNamedItem("id").getNodeValue());
-					this.users.removeUser(userId);
-				}
-				
-				Node map = (Node) mapExpr.evaluate(dom, XPathConstants.NODE);
-				if (map != null) {
-					NodeList points = (NodeList) pointExpr.evaluate(map, XPathConstants.NODESET);
-					GeoPoint[] geoPoints = new GeoPoint[points.getLength()];
-					for (int i = 0; i < points.getLength(); i++) {
-						GeoPoint gPoint = new GeoPoint(
-								(int) (Float.parseFloat(points.item(i).getAttributes().getNamedItem("lat").getNodeValue()) * 1E6),
-								(int) (Float.parseFloat(points.item(i).getAttributes().getNamedItem("lon").getNodeValue()) * 1E6)
-						);
-						geoPoints[i] = gPoint;
-					}
-					
-					route.setPoints(geoPoints);
-				}
-				
-				Node stats = (Node) statsExpr.evaluate(dom, XPathConstants.NODE);
-				if (stats != null) {
-					String l = stats.getChildNodes().item(0).getTextContent();
-					Message msg = new Message();
-					try {
-						float le = Float.parseFloat(l);
-						DecimalFormat df = new DecimalFormat("0.###");
-						msg.obj = "Zuglänge: " + df.format(le) + " km";
-					} catch (NumberFormatException e) {
-						msg.obj = "Zuglänge derzeit unbekannt";
-					}
-					this.length.sendMessage(msg);
-				}
+				parseData(dom);
 			} while (read >= 0);
 		} catch (ClientProtocolException e) {
 			e.printStackTrace();
@@ -196,11 +121,113 @@ public class HttpConnection extends Thread {
 		} catch (SAXException e) {
 			e.printStackTrace();
 			terminate();
+		}
+		getClient().getConnectionManager().shutdown();
+	}
+	
+	protected XPath getXPath() {
+		if (xPath == null) {
+			XPathFactory xfactory = XPathFactory.newInstance();
+			XPath x = xfactory.newXPath();
+			x.setNamespaceContext(new NamespaceContext() {
+				@Override
+				public Iterator<String> getPrefixes(String namespaceURI) {
+					return null;
+				}
+				
+				@Override
+				public String getPrefix(String namespaceURI) {
+					return null;
+				}
+				
+				@Override
+				public String getNamespaceURI(String prefix) {
+					if (prefix.equals("gpx")) return "http://www.topografix.com/GPX/1/1";
+					return null;
+				}
+			});
+		}
+		return xPath;
+	}
+	
+	protected void parseData(Document dom) {
+		try {
+			parseUserUpdates(dom);
+			parseUserRemovals(dom);
+			parseMapData(dom);
+			parseStatisticsUpdates(dom);
 		} catch (XPathExpressionException e) {
 			e.printStackTrace();
 			terminate();
 		}
-		getClient().getConnectionManager().shutdown();
+	}
+
+	private void parseStatisticsUpdates(Document dom) throws XPathExpressionException {
+		Node stats = (Node) statsExpression.evaluate(dom, XPathConstants.NODE);
+		if (stats != null) {
+			String l = stats.getChildNodes().item(0).getTextContent();
+			Message msg = new Message();
+			try {
+				float le = Float.parseFloat(l);
+				DecimalFormat df = new DecimalFormat("0.###");
+				msg.obj = "Zuglänge: " + df.format(le) + " km";
+			} catch (NumberFormatException e) {
+				msg.obj = "Zuglänge derzeit unbekannt";
+			}
+			this.length.sendMessage(msg);
+		}
+	}
+
+	private void parseMapData(Document dom) throws XPathExpressionException {
+		Node map = (Node) mapExpression.evaluate(dom, XPathConstants.NODE);
+		if (map != null) {
+			NodeList points = (NodeList) pointExpression.evaluate(map, XPathConstants.NODESET);
+			GeoPoint[] geoPoints = new GeoPoint[points.getLength()];
+			for (int i = 0; i < points.getLength(); i++) {
+				GeoPoint gPoint = new GeoPoint(
+						(int) (Float.parseFloat(points.item(i).getAttributes().getNamedItem("lat").getNodeValue()) * 1E6),
+						(int) (Float.parseFloat(points.item(i).getAttributes().getNamedItem("lon").getNodeValue()) * 1E6)
+				);
+				geoPoints[i] = gPoint;
+			}
+			
+			route.setPoints(geoPoints);
+		}
+	}
+
+	private void parseUserRemovals(Document dom) throws XPathExpressionException {
+		// get removals
+		NodeList users = (NodeList) quitExpression.evaluate(dom, XPathConstants.NODESET);
+		for (int i = 0; i < users.getLength(); i++) {
+			int userId = Integer.parseInt(users.item(i).getAttributes().getNamedItem("id").getNodeValue());
+			this.users.removeUser(userId);
+		}
+	}
+
+	private void parseUserUpdates(Document dom) throws XPathExpressionException {
+		// get location updates
+		NodeList users = (NodeList) userExpression.evaluate(dom, XPathConstants.NODESET);
+		for (int i = 0; i < users.getLength(); i++) {
+			Node location = (Node) locationExpression.evaluate(users.item(i), XPathConstants.NODE);
+			int lat = 0, lon = 0;
+			for (int k = 0; k < location.getChildNodes().getLength(); k++) {
+				Node coord = location.getChildNodes().item(k);
+				int value = (int) (Float.parseFloat(coord.getTextContent()) * 1E6);
+				if (coord.getNodeName().equals("lat")) lat = value;
+				if (coord.getNodeName().equals("lon")) lon = value;
+			}
+			GeoPoint point = new GeoPoint(lat, lon);
+			Node user = users.item(i);
+			int userId = Integer.parseInt(user.getAttributes().getNamedItem("id").getNodeValue());
+			UserOverlayItem o = this.users.getUser(userId);
+			if (o != null) {
+				o.setPoint(point);
+			} else {
+				String userName = user.getAttributes().getNamedItem("name").getNodeValue();
+				String team = user.getAttributes().getNamedItem("team").getNodeValue();
+				this.users.addUser(new UserOverlayItem(point, userId, userName, team));
+			}
+		}
 	}
 	
 	public void terminate() {
