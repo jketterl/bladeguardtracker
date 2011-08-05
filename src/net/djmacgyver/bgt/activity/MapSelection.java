63b4f55e71031553571eed8ad65614c29d938601
Jakob Ketterl <jketterl@chipxonio.de>
2011-08-05 16:21:44 +0200

    confirmation dialog first work

--- src/net/djmacgyver/bgt/activity/MapSelection.java
+++ src/net/djmacgyver/bgt/activity/MapSelection.java
@@ -2,7 +2,10 @@ package net.djmacgyver.bgt.activity;
 
 import net.djmacgyver.bgt.R;
 import net.djmacgyver.bgt.map.MapList;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.ListActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -10,6 +13,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 public class MapSelection extends ListActivity {
+	private final static int DIALOG_CONFIRM = 0;
 	private MapList maps;
 	
 	private MapList getMaps() {
@@ -33,6 +37,31 @@ public class MapSelection extends ListActivity {
 
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
-		System.out.println("clicked on map: " + id);
+		//System.out.println("clicked on map: " + id);
+		showDialog(DIALOG_CONFIRM);
+	}
+
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		switch (id) {
+			case DIALOG_CONFIRM:
+				AlertDialog.Builder builder = new AlertDialog.Builder(this);
+				builder.setTitle("are you sure?");
+				builder.setMessage("this will reset the map for all users...");
+				builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						dialog.dismiss();
+					}
+				});
+				builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						dialog.dismiss();
+					}
+				});
+				return builder.create();
+		}
+		return super.onCreateDialog(id);
 	}
 }
