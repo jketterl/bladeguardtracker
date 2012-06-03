5f5d2514a15b1a1f689f606d4cb1b72511f7b4cd
Jakob Ketterl <jketterl@chipxonio.de>
2012-06-03 23:10:01 +0200

    first work to disconnect after 5min

--- src/net/djmacgyver/bgt/socket/SocketService.java
+++ src/net/djmacgyver/bgt/socket/SocketService.java
@@ -2,12 +2,14 @@ package net.djmacgyver.bgt.socket;
 
 import java.util.Vector;
 
+import net.djmacgyver.bgt.keepalive.KeepAliveTarget;
+import net.djmacgyver.bgt.keepalive.KeepAliveThread;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
 
-public class SocketService extends Service {
+public class SocketService extends Service implements KeepAliveTarget {
 	// necessary for interaction with service clients
 	private Binder binder = new LocalBinder();
 	
@@ -25,6 +27,7 @@ public class SocketService extends Service {
 	// instance variables
 	private HttpSocketConnection sharedConn;
 	private Vector<Object> stakes = new Vector<Object>();
+	private KeepAliveThread socketTimeout;
 	
 	// public service methods
 	public HttpSocketConnection getSharedConnection(Object stake) {
@@ -52,5 +55,18 @@ public class SocketService extends Service {
 			sharedConn.connect();
 		}
 		return sharedConn;
+	}
+
+	private KeepAliveThread getSocketTimeout() {
+		if (socketTimeout == null) {
+			socketTimeout = new KeepAliveThread(this, 300);
+		}
+		return socketTimeout;
+	}
+
+	public void keepAlive(KeepAliveThread source) {
+		if (source == getSocketTimeout()) {
+			
+		}
 	}
 }
