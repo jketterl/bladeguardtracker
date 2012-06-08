998834aec8f03c116efd5f59527f662ffb874c7b
Jakob Ketterl <jketterl@chipxonio.de>
2012-06-08 11:21:04 +0200

    remove stakes debugging

--- src/net/djmacgyver/bgt/socket/SocketService.java
+++ src/net/djmacgyver/bgt/socket/SocketService.java
@@ -42,7 +42,6 @@ public class SocketService extends Service implements KeepAliveTarget {
 			socketTimeout = null;
 		}
 		stakes.add(obj);
-		System.out.println("stakes: " + stakes.size() + "; " + stakes);
 	}
 	
 	public void removeStake(Object obj) {
@@ -51,7 +50,6 @@ public class SocketService extends Service implements KeepAliveTarget {
 		if (stakes.isEmpty() && sharedConn != null) {
 			if (!getSocketTimeout().isAlive()) getSocketTimeout().start();
 		}
-		System.out.println("stakes: " + stakes.size() + "; " + stakes);
 	}
 
 	public HttpSocketConnection getSharedConnection() {
