619492ec60de0c19107d8de2d498ffe867760f5c
Jakob Ketterl <jketterl@chipxonio.de>
2012-06-15 16:43:55 +0200

    this variant should be visible, toot

--- src/net/djmacgyver/bgt/socket/HttpSocketConnection.java
+++ src/net/djmacgyver/bgt/socket/HttpSocketConnection.java
@@ -385,7 +385,7 @@ public class HttpSocketConnection {
 		}
 	}
 	
-	protected void sendCommand(String command, JSONObject data) {
+	public void sendCommand(String command, JSONObject data) {
 		synchronized (listeners) {
 			Iterator<HttpSocketListener> i = listeners.iterator();
 			while (i.hasNext()) i.next().receiveCommand(command, data);
