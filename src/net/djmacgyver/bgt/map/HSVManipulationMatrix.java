d1720aea542d8d79faf4a357b087abdd2515b34b
Jakob Ketterl <jketterl@chipxonio.de>
2012-08-03 16:00:53 +0200

    add lightness manipulation (kind of)

--- src/net/djmacgyver/bgt/map/HSVManipulationMatrix.java
+++ src/net/djmacgyver/bgt/map/HSVManipulationMatrix.java
@@ -1,16 +1,16 @@
 package net.djmacgyver.bgt.map;
 
 import android.graphics.ColorMatrix;
+import android.util.FloatMath;
 
 public class HSVManipulationMatrix extends ColorMatrix {
-	public HSVManipulationMatrix(float hue/*, float saturation, float value*/) {
+	public HSVManipulationMatrix(float hue/*, float saturation*/, float value) {
 		super();
-		float value = (hue % 360f) / 180f * (float) Math.PI;
-	    if (value == 0) return;
-	    System.out.println("input: " + hue + "; output: " + value);
+		float twist = (hue % 360f) / 180f * (float) Math.PI;
+	    if (twist == 0 && value == 1) return;
 
-	    float cosVal = (float) Math.cos(value);
-	    float sinVal = (float) Math.sin(value);
+	    float cosVal = FloatMath.cos(twist);
+	    float sinVal = FloatMath.sin(twist);
 	    /*
 	    float lumR = 0.33333f;
 	    float lumG = 0.33333f;
@@ -21,9 +21,9 @@ public class HSVManipulationMatrix extends ColorMatrix {
 	    float lumB = 0.072f;
 	    float[] mat = new float[]
 	    { 
-	            lumR + cosVal * (1 - lumR) + sinVal * (-lumR),    lumG + cosVal * (-lumG) + sinVal * (-lumG),     lumB + cosVal * (-lumB) + sinVal * (1 - lumB), 0, 0, 
-	            lumR + cosVal * (-lumR) + sinVal * (0.143f),      lumG + cosVal * (1 - lumG) + sinVal * (0.140f), lumB + cosVal * (-lumB) + sinVal * (-0.283f),  0, 0,
-	            lumR + cosVal * (-lumR) + sinVal * (-(1 - lumR)), lumG + cosVal * (-lumG) + sinVal * (lumG),      lumB + cosVal * (1 - lumB) + sinVal * (lumB),  0, 0, 
+	            (lumR + cosVal * (1 - lumR) + sinVal * (-lumR))    * value, (lumG + cosVal * (-lumG) + sinVal * (-lumG))     * value, (lumB + cosVal * (-lumB) + sinVal * (1 - lumB)) * value, 0, 0, 
+	            (lumR + cosVal * (-lumR) + sinVal * (0.143f))      * value, (lumG + cosVal * (1 - lumG) + sinVal * (0.140f)) * value, (lumB + cosVal * (-lumB) + sinVal * (-0.283f))  * value, 0, 0,
+	            (lumR + cosVal * (-lumR) + sinVal * (-(1 - lumR))) * value, (lumG + cosVal * (-lumG) + sinVal * (lumG))      * value, (lumB + cosVal * (1 - lumB) + sinVal * (lumB))  * value, 0, 0, 
 	            0f, 0f, 0f, 1f, 0f, 
 	            0f, 0f, 0f, 0f, 1f };
 		set(mat);
