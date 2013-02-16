1292c529ed879daaeb1a42fc1bc84f2b27880528
Jakob Ketterl <jketterl@chipxonio.de>
2013-02-16 16:30:28 +0100

    implement the new token-only authentication

--- src/net/djmacgyver/bgt/socket/command/FacebookLoginCommand.java
+++ src/net/djmacgyver/bgt/socket/command/FacebookLoginCommand.java
@@ -3,22 +3,13 @@ package net.djmacgyver.bgt.socket.command;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.facebook.model.GraphUser;
 
 public class FacebookLoginCommand extends AbstractAuthCommand {
-	public FacebookLoginCommand() {
+	public FacebookLoginCommand(String accessToken) {
 		super ("facebookLogin");
-	}
-	
-	public FacebookLoginCommand(GraphUser user) {
-		this();
-		setUser(user);
-	}
-	
-	public void setUser(GraphUser user) {
 		data = new JSONObject();
 		try {
-			this.data.put("userId", user.getId());
+			this.data.put("accessToken", accessToken);
 		} catch (JSONException e) {}
 	}
 }
