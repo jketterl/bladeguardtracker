d0cf6a0dbd7724622a4e77fd74fe4798c3373acf
Jakob Ketterl <jakob.ketterl@gmx.de>
2014-05-29 23:05:26 +0200

    preparing automatic connectivity

--- BladeguardTracker/src/main/java/net/djmacgyver/bgt/socket/HttpSocketConnection.java
+++ BladeguardTracker/src/main/java/net/djmacgyver/bgt/socket/HttpSocketConnection.java
@@ -24,11 +24,16 @@ import net.djmacgyver.bgt.socket.command.UnsubscribeUpdatesCommand;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources.NotFoundException;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.util.SparseArray;
@@ -36,6 +41,8 @@ import android.util.SparseArray;
 import com.codebutler.android_websockets.WebSocketClient;
 
 public class HttpSocketConnection {
+    private static final String TAG = "HttpSocketConnection";
+
 	public static final int STATE_DISCONNECTED = 0;
 	public static final int STATE_CONNECTING = 1;
 	public static final int STATE_CONNECTED = 2;
@@ -47,14 +54,26 @@ public class HttpSocketConnection {
 	private SparseArray<SocketCommand> requests = new SparseArray<SocketCommand>();
 	private LinkedList<SocketCommand> queue;
 	private final ArrayList<HttpSocketListener> listeners = new ArrayList<HttpSocketListener>();
-	//private ArrayList<String> subscribed = new ArrayList<String>();
 	private HashMap <Event, ArrayList<String>> subscribed = new HashMap<Event, ArrayList<String>>();
 	private AbstractAuthCommand authentication;
 	private ArrayList<SocketCommandCallback> authCallbacks = new ArrayList<SocketCommandCallback>();
+
+    private BroadcastReceiver connectivityReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d(TAG, "connectivity: " + isConnected());
+        }
+
+        private boolean isConnected() {
+            ConnectivityManager c = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo i = c.getActiveNetworkInfo();
+            return i != null && i.isConnected();
+        }
+    };
 	
 	private int state = STATE_DISCONNECTED;
 	
-	public HttpSocketConnection(Context applicationContext) {
+	protected HttpSocketConnection(Context applicationContext) {
 		this.context = applicationContext;
 	}
 	
@@ -63,177 +82,11 @@ public class HttpSocketConnection {
 		if (socket == null) {
 			try {
 				URI url = new URI(context.getResources().getString(R.string.websocket));
-				socket = new WebSocketClient(url, new WebSocketClient.Handler() {
-					private boolean valid = true;
-					
-					@Override
-					public void onMessage(byte[] data) {
-						if (!valid) return;
-						System.out.println("OMG! Binary data!");
-					}
-					
-					@Override
-					public void onMessage(String message) {
-						if (!valid) return;
-						if (message.length() == 0) return;
-						try {
-							JSONObject response = new JSONObject(message);
-							if (response.has("requestId")) {
-								Integer id = response.getInt("requestId");
-								SocketCommand request = requests.get(id);
-								if (request != null) {
-									request.updateResult(response);
-									requests.remove(id);
-								} else {
-									Log.e("SocketConnection", "received response for unknown command id: " + id);
-								}
-							} else if (response.has("event") && response.getString("event").equals("update")) {
-								if (response.has("data")) {
-									sendUpdate(response.getJSONObject("data"));
-								} else {
-									System.out.println("received message without data!");
-								}
-							} else if (response.has("command")) {
-								JSONObject data = response.has("data") ? response.getJSONObject("data") : new JSONObject();
-								sendCommand(response.getString("command"), data);
-							}
-						} catch (JSONException e) {
-							// propably an old XML message. ignore... for now we only support json
-							System.out.println("unable to parse message: " + message);
-						}
-						checkDisconnect();
-					}
-					
-					@Override
-					public void onError(Exception error) {
-						if (!valid) return;
-												
-						System.out.println("Error on websocket connection!");
-						error.printStackTrace();
-						
-						// try disconnecting the current socket (if not already disconnected)
-						if (queue == null) try {
-							// if the disconnect succeeds it should call onDisconnect()
-							socket.disconnect();
-						} catch (Exception ignored) {}
-						// we can never be sure whether onDisconnect() is called, so to be sure we call it at least 
-						// once here.
-						// onDisconnect() is protected against double execution using valid
-						onDisconnect(-1, error.getMessage());
-					}
-					
-					private void reconnect(){
-						System.out.println("reconnect()");
-
-						// put up a new queue
-						// this also prevents new commands from being sent
-						if (queue == null) queue = new LinkedList<SocketCommand>();
-						
-						// send state notification
-						setState(STATE_CONNECTING);
-						
-						// give the server 10s grace time
-						try {
-							Thread.sleep(10000);
-						} catch (InterruptedException e) {
-							System.out.println("interrupted!");
-						}
-						
-						// we might have changed our mind while we were sleeping...
-						if (doDisconnect) {
-							doDisconnect = false;
-							return;
-						}
-						
-						// if we get to here, we really want that connection to be back up
-						getSocket().connect();
-					}
-					
-					@Override
-					public void onDisconnect(int code, String reason) {
-						if (!valid) return;
-						
-						// invalidate this handler 
-						valid = false;
-						
-						// invalidate authentication (must be re-sent on the next connect)
-						authentication = null;
-						
-						// send state updates
-						setState(STATE_DISCONNECTED);
-						socket = null;
-						
-						// we got disconnected.
-						System.out.println("disconnected");
-
-						// cancel all outstanding requests (they won't receive data on a closed socket anyway)
-						cancelRequests();
-						
-						// the disconnection was intentional - ok. accept it and leave it that way.
-						if (doDisconnect) {
-							doDisconnect = false;
-							return;
-						}
-						
-						// otherwise: try to reconnect
-						reconnect();
-					}
-					
-					@Override
-					public void onConnect() {
-						if (!valid) return;
-						System.out.println("connected");
-						
-						synchronized (queue) {
-							// first things first: send handshake & authentication.
-							sendHandshake();
-							SocketCommandCallback r = new SocketCommandCallback() {
-								@Override
-								public void run(SocketCommand command) {
-									// get the current queue
-									LinkedList<SocketCommand> q = queue;
-									queue = null;
-									
-									// send all queued commands
-									while (!q.isEmpty()) sendCommand(q.poll());
-								}
-							};
-							authenticate(r);
-							//if (authentication != null) authentication.addCallback(r); else r.run();
-						}
-						
-						sendSubscriptions();
-						
-						setState(STATE_CONNECTED);
-						
-						// check whether this connection is eligible for disconnection
-						checkDisconnect();
-					}
-
-				}, null);
+				socket = new WebSocketClient(url, new WebSocketHandler(), null);
 				WebSocketClient.setTrustManagers(new TrustManager[]{
-						new X509TrustManager() {
-							@Override
-							public X509Certificate[] getAcceptedIssuers() {
-								// TODO Auto-generated method stub
-								return null;
-							}
-							
-							@Override
-							public void checkServerTrusted(X509Certificate[] chain, String authType)
-									throws CertificateException {
-								// TODO Auto-generated method stub
-								
-							}
-							
-							@Override
-							public void checkClientTrusted(X509Certificate[] chain, String authType)
-									throws CertificateException {
-								// TODO Auto-generated method stub
-								
-							}
-						}
+                        new AcceptAllTrustManager()
 				});
+                context.registerReceiver(connectivityReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
 			} catch (NotFoundException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -358,6 +211,7 @@ public class HttpSocketConnection {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+        context.unregisterReceiver(connectivityReceiver);
 	}
 
 	private ArrayList<String> getEventSubscriptions(Event event) {
@@ -454,4 +308,175 @@ public class HttpSocketConnection {
 			authCallbacks.add(c);
 		}
 	}
+
+    private static class AcceptAllTrustManager implements X509TrustManager {
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        @Override
+        public void checkServerTrusted(X509Certificate[] chain, String authType)
+                throws CertificateException {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void checkClientTrusted(X509Certificate[] chain, String authType)
+                throws CertificateException {
+            // TODO Auto-generated method stub
+
+        }
+    }
+
+    private class WebSocketHandler implements WebSocketClient.Handler {
+        private boolean valid = true;
+
+        @Override
+        public void onMessage(byte[] data) {
+            if (!valid) return;
+            System.out.println("OMG! Binary data!");
+        }
+
+        @Override
+        public void onMessage(String message) {
+            if (!valid) return;
+            if (message.length() == 0) return;
+            try {
+                JSONObject response = new JSONObject(message);
+                if (response.has("requestId")) {
+                    Integer id = response.getInt("requestId");
+                    SocketCommand request = requests.get(id);
+                    if (request != null) {
+                        request.updateResult(response);
+                        requests.remove(id);
+                    } else {
+                        Log.e("SocketConnection", "received response for unknown command id: " + id);
+                    }
+                } else if (response.has("event") && response.getString("event").equals("update")) {
+                    if (response.has("data")) {
+                        sendUpdate(response.getJSONObject("data"));
+                    } else {
+                        System.out.println("received message without data!");
+                    }
+                } else if (response.has("command")) {
+                    JSONObject data = response.has("data") ? response.getJSONObject("data") : new JSONObject();
+                    sendCommand(response.getString("command"), data);
+                }
+            } catch (JSONException e) {
+                // propably an old XML message. ignore... for now we only support json
+                System.out.println("unable to parse message: " + message);
+            }
+            checkDisconnect();
+        }
+
+        @Override
+        public void onError(Exception error) {
+            if (!valid) return;
+
+            System.out.println("Error on websocket connection!");
+            error.printStackTrace();
+
+            // try disconnecting the current socket (if not already disconnected)
+            if (queue == null) try {
+                // if the disconnect succeeds it should call onDisconnect()
+                socket.disconnect();
+            } catch (Exception ignored) {}
+            // we can never be sure whether onDisconnect() is called, so to be sure we call it at least
+            // once here.
+            // onDisconnect() is protected against double execution using valid
+            onDisconnect(-1, error.getMessage());
+        }
+
+        private void reconnect(){
+            System.out.println("reconnect()");
+
+            // put up a new queue
+            // this also prevents new commands from being sent
+            if (queue == null) queue = new LinkedList<SocketCommand>();
+
+            // send state notification
+            setState(STATE_CONNECTING);
+
+            // give the server 10s grace time
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                System.out.println("interrupted!");
+            }
+
+            // we might have changed our mind while we were sleeping...
+            if (doDisconnect) {
+                doDisconnect = false;
+                return;
+            }
+
+            // if we get to here, we really want that connection to be back up
+            getSocket().connect();
+        }
+
+        @Override
+        public void onDisconnect(int code, String reason) {
+            if (!valid) return;
+
+            // invalidate this handler
+            valid = false;
+
+            // invalidate authentication (must be re-sent on the next connect)
+            authentication = null;
+
+            // send state updates
+            setState(STATE_DISCONNECTED);
+            socket = null;
+
+            // we got disconnected.
+            System.out.println("disconnected");
+
+            // cancel all outstanding requests (they won't receive data on a closed socket anyway)
+            cancelRequests();
+
+            // the disconnection was intentional - ok. accept it and leave it that way.
+            if (doDisconnect) {
+                doDisconnect = false;
+                return;
+            }
+
+            // otherwise: try to reconnect
+            reconnect();
+        }
+
+        @Override
+        public void onConnect() {
+            if (!valid) return;
+            System.out.println("connected");
+
+            synchronized (queue) {
+                // first things first: send handshake & authentication.
+                sendHandshake();
+                SocketCommandCallback r = new SocketCommandCallback() {
+                    @Override
+                    public void run(SocketCommand command) {
+                        // get the current queue
+                        LinkedList<SocketCommand> q = queue;
+                        queue = null;
+
+                        // send all queued commands
+                        while (!q.isEmpty()) sendCommand(q.poll());
+                    }
+                };
+                authenticate(r);
+                //if (authentication != null) authentication.addCallback(r); else r.run();
+            }
+
+            sendSubscriptions();
+
+            setState(STATE_CONNECTED);
+
+            // check whether this connection is eligible for disconnection
+            checkDisconnect();
+        }
+
+    }
 }
